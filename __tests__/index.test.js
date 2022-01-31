d3892a87177eac741a28f768914ec173c7285665
Nikita Naumenko <naumenkoniki@gmail.com>
2022-01-31 21:35:48 +0300

    finish first step

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,28 @@
-import {describe, expect, test} from '@jest/globals'
+import { describe, expect, test } from '@jest/globals';
+import makeRouter from '../index.js';
 
-test('to ve true', () => {
-  expect(true).toBe(true)
-})
+const routes = [
+  {
+    path: '/courses',
+    handler: () => 'courses!',
+  },
+  {
+    path: '/courses/basics',
+    handler: () => 'basics',
+  },
+];
+
+const router = makeRouter(routes);
+
+describe('serve existed routes', () => {
+  test('correct response', () => {
+    const handler = router.serve('/courses');
+    expect(handler()).toBe('courses!');
+  });
+});
+
+describe('serve wrong routes', () => {
+  test('throw error', () => {
+    expect(() => router.serve('/404')).toThrow();
+  });
+});
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('123')
+const buildRouter = (routes) => (
+  {
+    serve: (outerRoute) => {
+      const route = routes.find(({ path }) => path === outerRoute);
+      if (!route) {
+        throw new Error();
+      }
+      return route.handler;
+    },
+  }
+);
+
+export default (routes) => {
+  const router = buildRouter(routes);
+  return router;
+};
